5e6d3d1d22d889df11e6001128386b534b7f198b
kelly brevin <kellybrevin83@gmail.com>
2025-04-13 23:57:46 +0300

    new commit

---
+++ client/script.js
@@ -0,0 +1,7 @@
+//get userinputs,buttons and store them in variables
+const addTodo = document.getElementById("addTodo");
+//figure out the link between the client and server
+//add event listeners for the buttons
+//functions to send requests to server and display response
+//figure out how to dynamically display the tasks/update and delete
+//figure out how to display responses and have them styled(DOM)
